dbd4bfba61d38cd246c776d328c6cec173b510b5
Apoorva Raj <apoorvaraj97@gmail.com>
2019-01-30 13:00:16 +0530

    Order Screen UI completed

--- src/Order.js
+++ src/Order.js
@@ -1,36 +1,22 @@
-import React from 'react'
-import { TouchableOpacity, Text, View } from 'react-native'
-import { Actions } from 'react-native-router-flux'
-import { Card, ListItem, Button, Icon } from 'react-native-elements';
+import React from 'react';
+import { View } from 'react-native';
+import ItemsContainer from './ItemsContainer';
+import Header from './Header.js';
+import BasketComponent from './BasketComponent'
+import Footer from './Footer'
+// import { Actions } from 'react-native-router-flux'
 
-import NumberPicker from './NumberPicker'
 const Order = () => {
-
-
    return (
-      // <TouchableOpacity style = {{ margin: 128 }} >
-      //    <Text>Your Order</Text>
-      // </TouchableOpacity>
-      <View>
-      <Card
-    title='Cart'>
-    <Text style={{marginBottom: 20}}>
-      Your orders :
-    </Text>
-    <View
-    style={{
-          flex: 1,
-         flexDirection: 'row',
-         height: 50,
-         padding: 20,
-       }}>
-    <Text>
-      Item 1
-    </Text>
-    <NumberPicker />
-    </View>
-  </Card>
+     <View style={{ flex: 1 }}>
+        <Header />
+        <ItemsContainer />
+        <BasketComponent />
+        <Footer />
       </View>
    )
 }
+
+
+
 export default Order
